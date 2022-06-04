94d4a4d857ec1b0b44a7abaef1b1a30d730985f0
Almas <almas1610@gmail.com>
2022-06-05 04:31:18 +0600

    feature

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,64 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const [counter, setCounter] = React.useState(0)
+  const [start, setStart] = React.useState(0);
+  const [maxValue, setMaxValue] = React.useState(0);
+
+  let val: any;
+  const startWith = (start: any) => {
+    val = start;
+  }
+
+  const startWithButton = () => {
+    setStart(val)
+    setCounter(val)
+  }
+
+  const inc = () => {
+    setCounter(counter + 1)
+  }
+
+  const reset = () => {
+    setCounter(0)
+  }
+
+  let stop: boolean = false;
+
+  if (counter === maxValue) {
+    stop = true;
+  }
+
+  const check = (val: any): boolean => {
+    if (val === maxValue) {
+      return true
+    }
+    return false
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='one_box'>
+        <div className='values'>
+          <h2>MAX VALUE: {maxValue}</h2>
+          <input className='superInput' type="text" onChange={({ target }) => setMaxValue(parseInt(target.value))} />
+          <h2>START VALUE: {start}</h2>
+          <input className='superInput' type="text" onChange={({ target }) => startWith(parseInt(target.value))} />
+        </div>
+        <div>
+          <button onClick={startWithButton}>SET</button>
+        </div>
+      </div>
+      <div className='two-box'>
+        <div className='values'>
+          <h1>{counter}</h1>
+        </div>
+        <div>
+          <button disabled={check(counter)} onClick={inc}>INC</button>
+          <button onClick={reset}>RESET</button>
+        </div>
+      </div>
     </div>
   );
 }
